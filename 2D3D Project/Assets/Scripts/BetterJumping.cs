13aaa892da2ed2017af490fc7da63ffb05b53674
cachi01 <61888285+cachi01@users.noreply.github.com>
2020-09-20 21:29:40 -0500

    Cachi Semana 1 Entrega 1
    -Script de movimiento y salto
    -Escena "Cachi"
    -Escena "BillboardTest"

---
+++ 2D3D Project/Assets/Scripts/BetterJumping.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//THIS SCRIPT MANIPULATES THE JUMP DISTANCE OF THE PLAYER BASED ON HOW LONG THEY PRESSED THE JUMP BUTTON
+
+public class BetterJumping : MonoBehaviour
+{
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private bool jumpPressed;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //We handle player input in Update
+    void Update()
+    {
+        jumpPressed = Input.GetButton("Jump");
+    }
+
+    //And we handle everything physics related in this FixedUpdate
+    private void FixedUpdate()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !jumpPressed)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
---
+++ 2D3D Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Range(1, 100)]
+    public float jumpVelocity;
+
+    [Range(1, 20)]
+    public float movementSpeed;
+
+    public float GroundDistance = 0.2f;
+    public LayerMask Ground;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+    private Transform groundChecker;
+    private Vector3 movementInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //The first child should always be an empty object. We'll use this to check if the player is touching the ground.
+        groundChecker = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        //Checks if the character is grounded.
+        isGrounded = Physics.CheckSphere(groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        movementInput = Vector3.zero;
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.z = Input.GetAxisRaw("Vertical");
+        if (movementInput != Vector3.zero)
+        {
+            transform.forward = movementInput;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //ClampMagnitude clamps the magnitude of the vector so that diagonal movement won't be faster than horizontal or vertical movement.
+        rb.MovePosition(rb.position + Vector3.ClampMagnitude(movementInput * movementSpeed, 6f) * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        rb.velocity = Vector3.up * jumpVelocity;
+    }
+}
