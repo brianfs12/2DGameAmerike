fec33f019565800948ed810aad561e312008b05d
cachi01 <61888285+cachi01@users.noreply.github.com>
2020-11-01 13:54:44 -0600

    BulletSpawner and Level Transition system

---
+++ 2D3D Project/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletUp;
+    public GameObject bulletDown;
+    public GameObject bulletLeft;
+    public GameObject bulletRight;
+
+    public float shootCooldown = 3f;
+
+    public bool canShoot = true;
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canShoot)
+        {
+            StartCoroutine(SpawnBullet());
+        }
+
+    }
+
+    IEnumerator SpawnBullet()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shootCooldown);
+        if(up)
+        {
+            Instantiate(bulletUp, transform.position, transform.rotation);
+        }
+        if (down)
+        {
+            Instantiate(bulletDown, transform.position, transform.rotation);
+        }
+        if (right)
+        {
+            Instantiate(bulletRight, transform.position, transform.rotation);
+        }
+        if (left)
+        {
+            Instantiate(bulletLeft, transform.position, transform.rotation);
+        }
+        canShoot = true;
+
+    }
+}
---
+++ 2D3D Project/Assets/Scripts/BulletUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletUp : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+    }
+}
